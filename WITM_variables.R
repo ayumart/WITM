bc4b704223cd79085c9bac65995c4fdbe3775abc
NATALIA DEBANDI <nataliadebandi@gmail.com>
2024-09-23 10:38:10 -0300

    actualizando

--- WITM_variables.R
+++ WITM_variables.R
@@ -12,151 +12,120 @@ witm$date <-  as.Date(witm$end,'%Y-%m-%d')
 
 witm$date_s <-  paste(format(as.Date(witm$date,format="%Y-%m-%d"), format = "%m"), format(as.Date(witm$date,format="%Y-%m-%d"), format = "%d"), sep ="-", collapse=NULL)
 
-witm<-witm %>% 
-  mutate(q9_year_formation_agrup=case_when(
-    q9_year_formation>=2021 & q9_year_formation<=2023 ~  "(1) 2021-2023",
-    q9_year_formation<2021 & q9_year_formation>2014 ~  "(2) From 2015 to 2020",
-    q9_year_formation<2015 & q9_year_formation>2009 ~  "(3) From 2010 to 2014",
-    q9_year_formation<2010 & q9_year_formation>1999 ~  "(4) From 2000 to 2009",
-    q9_year_formation<=2000 & q9_year_formation>1945 ~  "(5) Before 2000",
-    is.na(q9_year_formation) ~ NA,
-    TRUE ~  NA)) 
 
 
-witm<-witm %>%
-  mutate(q10_budget_year_2021=case_when(
-    q10_budget_year_2021=="zero_budget" ~  "(a) Zero budget",
-    q10_budget_year_2021=="less_than_5000_usd" ~  "(b) <5000",
-    q10_budget_year_2021=="between_5001_and_10000_usd" ~  "(c) 5001-10000",
-    q10_budget_year_2021=="between_10001_and_30000_usd" ~  "(d) 10001-30000",
-    q10_budget_year_2021=="between_30001_and_50000_usd" ~  "(e) 30001 -50000",
-    q10_budget_year_2021=="between_50001_and_100000_usd" ~  "(f) 50001 - 100000",
-    q10_budget_year_2021=="between_100001_and_250000_usd" ~  "(g) 100001 - 250000",
-    q10_budget_year_2021=="between_250001_and_500000_usd" ~  "(h) 250001 - 500000",
-    q10_budget_year_2021=="between_500001_and_1000000_usd" ~  "(i) 500001 - 1000000",
-    q10_budget_year_2021=="between_1000001_and_2000000_usd" ~  "(j) 1000001 - 2000000",
-    q10_budget_year_2021=="between_2000001_and_4000000_usd" ~  "(k) 2000001 - 4000000",
-    q10_budget_year_2021=="greater_than_4000001_usd" ~  "(l) > 4000001",
-    TRUE ~  NA))
-
-witm<-witm %>%
-  mutate(q10_budget_year_2022=case_when(
-    q10_budget_year_2022=="zero_budget" ~  "(a) Zero budget",
-    q10_budget_year_2022=="less_than_5000_usd" ~  "(b) <5000",
-    q10_budget_year_2022=="between_5001_and_10000_usd" ~  "(c) 5001-10000",
-    q10_budget_year_2022=="between_10001_and_30000_usd" ~  "(d) 10001-30000",
-    q10_budget_year_2022=="between_30001_and_50000_usd" ~  "(e) 30001 -50000",
-    q10_budget_year_2022=="between_50001_and_100000_usd" ~  "(f) 50001 - 100000",
-    q10_budget_year_2022=="between_100001_and_250000_usd" ~  "(g) 100001 - 250000",
-    q10_budget_year_2022=="between_250001_and_500000_usd" ~  "(h) 250001 - 500000",
-    q10_budget_year_2022=="between_500001_and_1000000_usd" ~  "(i) 500001 - 1000000",
-    q10_budget_year_2022=="between_1000001_and_2000000_usd" ~  "(j) 1000001 - 2000000",
-    q10_budget_year_2022=="between_2000001_and_4000000_usd" ~  "(k) 2000001 - 4000000",
-    q10_budget_year_2022=="greater_than_4000001_usd" ~  "(l) > 4000001",
-    TRUE ~  NA))
-
-witm<-witm %>%
-  mutate(q10_budget_year_2023=case_when(
-    q10_budget_year_2023=="zero_budget" ~  "(a) Zero budget",
-    q10_budget_year_2023=="less_than_5000_usd" ~  "(b) <5000",
-    q10_budget_year_2023=="between_5001_and_10000_usd" ~  "(c) 5001-10000",
-    q10_budget_year_2023=="between_10001_and_30000_usd" ~  "(d) 10001-30000",
-    q10_budget_year_2023=="between_30001_and_50000_usd" ~  "(e) 30001 -50000",
-    q10_budget_year_2023=="between_50001_and_100000_usd" ~  "(f) 50001 - 100000",
-    q10_budget_year_2023=="between_100001_and_250000_usd" ~  "(g) 100001 - 250000",
-    q10_budget_year_2023=="between_250001_and_500000_usd" ~  "(h) 250001 - 500000",
-    q10_budget_year_2023=="between_500001_and_1000000_usd" ~  "(i) 500001 - 1000000",
-    q10_budget_year_2023=="between_1000001_and_2000000_usd" ~  "(j) 1000001 - 2000000",
-    q10_budget_year_2023=="between_2000001_and_4000000_usd" ~  "(k) 2000001 - 4000000",
-    q10_budget_year_2023=="greater_than_4000001_usd" ~  "(l) > 4000001",
-    TRUE ~  NA))
-
-
-# 1.	Lesbian, bisexual and queer rights + trans and non-binary rights + intersex rights = LBQTI+
-# 2.	Young feminist + girls’ movements 
-# 3.	Sex workers rights
-# 4.	Religious & ethnic minority rights + anti-caste
-# 5.	Climate and environmental justice 
-# 6.	Countering anti-gender & anti-rights 
-# 7.	Anti-militarization and peace + crisis response 
-# 8.	Harm reduction + resisting the war on drugs
-
-witm<-witm %>% 
-  mutate(q4_awid_LGBTIQ=case_when(q4_forms_organizing.lesbian_bisexual_queer_rights==1 | q4_forms_organizing.trans_non_binary_rights==1 |
-                                    q4_forms_organizing.intersex_rights==1 ~1, TRUE ~0)) %>% 
-  mutate(q4_awid_young=case_when(q4_forms_organizing.young_feminists==1 | q4_forms_organizing.girls_movements==1 ~1, TRUE ~0)) %>%
-  mutate(q4_awid_sex=case_when(q4_forms_organizing.sex_workers_rights==1 ~1, TRUE ~0)) %>%
-  mutate(q4_awid_nin=case_when(q4_forms_organizing.religious_ethnic_minority_rights==1 |q4_forms_organizing.anti_caste==1~1, TRUE ~0)) %>%
-  mutate(q4_awid_climate=case_when(q4_forms_organizing.climate_justice ==1 ~1, TRUE ~0)) %>%
-  mutate(q4_awid_antig=case_when(q4_forms_organizing.countering_anti_gender==1 ~1, TRUE ~0)) %>%
-  mutate(q4_awid_crisis=case_when(q4_forms_organizing.anti_militarization==1 | q4_forms_organizing.crisis_response==1 ~1, TRUE ~0)) %>%
-  mutate(q4_awid_harm=case_when(q4_forms_organizing.harm_reduction==1 |q4_forms_organizing.resisting_war_on_drugs==1  ~1, TRUE ~0))
+# witm<-witm %>% 
+#   mutate(q9_year_formation_agrup=case_when(
+#     q9_year_formation>=2021 & q9_year_formation<=2023 ~  "(1) 2021-2023",
+#     q9_year_formation<2021 & q9_year_formation>2014 ~  "(2) From 2015 to 2020",
+#     q9_year_formation<2015 & q9_year_formation>2009 ~  "(3) From 2010 to 2014",
+#     q9_year_formation<2010 & q9_year_formation>1999 ~  "(4) From 2000 to 2009",
+#     q9_year_formation<=2000 & q9_year_formation>1945 ~  "(5) Before 2000",
+#     is.na(q9_year_formation) ~ NA,
+#     TRUE ~  NA)) 
+# 
+# 
+# witm<-witm %>%
+#   mutate(q10_budget_year_2021=case_when(
+#     q10_budget_year_2021=="zero_budget" ~  "(a) Zero budget",
+#     q10_budget_year_2021=="less_than_5000_usd" ~  "(b) <5000",
+#     q10_budget_year_2021=="between_5001_and_10000_usd" ~  "(c) 5001-10000",
+#     q10_budget_year_2021=="between_10001_and_30000_usd" ~  "(d) 10001-30000",
+#     q10_budget_year_2021=="between_30001_and_50000_usd" ~  "(e) 30001 -50000",
+#     q10_budget_year_2021=="between_50001_and_100000_usd" ~  "(f) 50001 - 100000",
+#     q10_budget_year_2021=="between_100001_and_250000_usd" ~  "(g) 100001 - 250000",
+#     q10_budget_year_2021=="between_250001_and_500000_usd" ~  "(h) 250001 - 500000",
+#     q10_budget_year_2021=="between_500001_and_1000000_usd" ~  "(i) 500001 - 1000000",
+#     q10_budget_year_2021=="between_1000001_and_2000000_usd" ~  "(j) 1000001 - 2000000",
+#     q10_budget_year_2021=="between_2000001_and_4000000_usd" ~  "(k) 2000001 - 4000000",
+#     q10_budget_year_2021=="greater_than_4000001_usd" ~  "(l) > 4000001",
+#     TRUE ~  NA))
+# 
+# witm<-witm %>%
+#   mutate(q10_budget_year_2022=case_when(
+#     q10_budget_year_2022=="zero_budget" ~  "(a) Zero budget",
+#     q10_budget_year_2022=="less_than_5000_usd" ~  "(b) <5000",
+#     q10_budget_year_2022=="between_5001_and_10000_usd" ~  "(c) 5001-10000",
+#     q10_budget_year_2022=="between_10001_and_30000_usd" ~  "(d) 10001-30000",
+#     q10_budget_year_2022=="between_30001_and_50000_usd" ~  "(e) 30001 -50000",
+#     q10_budget_year_2022=="between_50001_and_100000_usd" ~  "(f) 50001 - 100000",
+#     q10_budget_year_2022=="between_100001_and_250000_usd" ~  "(g) 100001 - 250000",
+#     q10_budget_year_2022=="between_250001_and_500000_usd" ~  "(h) 250001 - 500000",
+#     q10_budget_year_2022=="between_500001_and_1000000_usd" ~  "(i) 500001 - 1000000",
+#     q10_budget_year_2022=="between_1000001_and_2000000_usd" ~  "(j) 1000001 - 2000000",
+#     q10_budget_year_2022=="between_2000001_and_4000000_usd" ~  "(k) 2000001 - 4000000",
+#     q10_budget_year_2022=="greater_than_4000001_usd" ~  "(l) > 4000001",
+#     TRUE ~  NA))
+# 
+# witm<-witm %>%
+#   mutate(q10_budget_year_2023=case_when(
+#     q10_budget_year_2023=="zero_budget" ~  "(a) Zero budget",
+#     q10_budget_year_2023=="less_than_5000_usd" ~  "(b) <5000",
+#     q10_budget_year_2023=="between_5001_and_10000_usd" ~  "(c) 5001-10000",
+#     q10_budget_year_2023=="between_10001_and_30000_usd" ~  "(d) 10001-30000",
+#     q10_budget_year_2023=="between_30001_and_50000_usd" ~  "(e) 30001 -50000",
+#     q10_budget_year_2023=="between_50001_and_100000_usd" ~  "(f) 50001 - 100000",
+#     q10_budget_year_2023=="between_100001_and_250000_usd" ~  "(g) 100001 - 250000",
+#     q10_budget_year_2023=="between_250001_and_500000_usd" ~  "(h) 250001 - 500000",
+#     q10_budget_year_2023=="between_500001_and_1000000_usd" ~  "(i) 500001 - 1000000",
+#     q10_budget_year_2023=="between_1000001_and_2000000_usd" ~  "(j) 1000001 - 2000000",
+#     q10_budget_year_2023=="between_2000001_and_4000000_usd" ~  "(k) 2000001 - 4000000",
+#     q10_budget_year_2023=="greater_than_4000001_usd" ~  "(l) > 4000001",
+#     TRUE ~  NA))
 
 
-witm<-witm %>%
+# 
+# 
+# 
+ base<-witm %>% 
+ filter(q0_consent=="yes")
+
+base<- base %>%
   mutate(q4_awid_focus=case_when
          (
-           (q4_awid_LGBTIQ==1 |
-              q4_awid_young==1 |
-              q4_awid_sex==1  |
-              q4_awid_nin==1 |
-              q4_awid_climate==1 |
-              q4_awid_antig==1 |
-              q4_awid_crisis==1 |
-              q4_awid_harm==1) ~1, TRUE ~0))  
+           (q4_forms_organizing.lesbian_bisexual_queer_rights==1 |
+              q4_forms_organizing.sex_workers_rights==1  |
+              q4_forms_organizing.trans_non_binary_rights==1 |
+              q4_forms_organizing.anti_caste==1 |
+              q4_forms_organizing.climate_justice==1 |
+              q4_forms_organizing.countering_anti_gender==1 |
+              q4_forms_organizing.anti_militarization==1 |
+              q4_forms_organizing.holistic_safety_protection_collective_care==1) ~1, TRUE ~0))  
 
-witm<-witm %>%
+base<- base %>%
   mutate(q4_awid_focus_no=case_when
          (q4_awid_focus!=1 ~ 1, TRUE ~0))  
 
 
-# 
-# witm<-witm %>%
-#   mutate(q4_grouped_feminist=case_when
-#          (
-#            (q4_forms_organizing.womens_rights==1 |
-#               q4_forms_organizing.human_rights==1  |
-#               q4_forms_organizing.end_gender_violence ==1  |
-#               q4_forms_organizing.srhr_bodily_autonomy==1) ~1, TRUE ~0)) %>%  
-#   
-#   mutate(q4_grouped_minorities=case_when
-#          (
-#            (
-#              q4_forms_organizing.young_feminists==1 |
-#                q4_forms_organizing.girls_movements ==1 |
-#                q4_forms_organizing.disability_rights==1 |
-#                q4_forms_organizing.lesbian_bisexual_queer_rights==1 |
-#                q4_forms_organizing.sex_workers_rights==1 |
-#                q4_forms_organizing.rights_for_people_living_with_hiv==1 |
-#                q4_forms_organizing.displaced_migrant_refugee_rights==1 |
-#                q4_forms_organizing.trans_non_binary_rights==1 |
-#                q4_forms_organizing.intersex_rights==1 |
-#                q4_forms_organizing.labour_rights==1 |
-#                q4_forms_organizing.indigenous_rights==1 |
-#                q4_forms_organizing.black_rights==1 |
-#                q4_forms_organizing.anti_caste==1 |
-#                q4_forms_organizing.hr_defenders_at_risk==1 |
-#                q4_forms_organizing.religious_ethnic_minority_rights==1 ) ~1, TRUE ~0)) %>% 
+
+base<- base %>%
+  mutate(q4_grouped_feminist=case_when
+         (
+           (q4_forms_organizing.womens_rights==1 |
+              q4_forms_organizing.human_rights==1  |
+              q4_forms_organizing.end_gender_violence ==1  |
+              q4_forms_organizing.srhr_bodily_autonomy==1) ~1, TRUE ~0)) %>%  
+  
+  mutate(q4_grouped_minorities=case_when
+         (
+           (
+             q4_forms_organizing.young_feminists==1 |
+               q4_forms_organizing.girls_movements ==1 |
+               q4_forms_organizing.disability_rights==1 |
+               q4_forms_organizing.lesbian_bisexual_queer_rights==1 |
+               q4_forms_organizing.sex_workers_rights==1 |
+               q4_forms_organizing.rights_for_people_living_with_hiv==1 |
+               q4_forms_organizing.displaced_migrant_refugee_rights==1 |
+               q4_forms_organizing.trans_non_binary_rights==1 |
+               q4_forms_organizing.intersex_rights==1 |
+               q4_forms_organizing.labour_rights==1 |
+               q4_forms_organizing.indigenous_rights==1 |
+               q4_forms_organizing.black_rights==1 |
+               q4_forms_organizing.anti_caste==1 |
+               q4_forms_organizing.hr_defenders_at_risk==1 |
+               q4_forms_organizing.religious_ethnic_minority_rights==1 ) ~1, TRUE ~0)) %>% 
   
   
-<<<<<<< HEAD
-  # mutate(q4_grouped_focus=case_when
-  #        (
-  #          (
-  #            q4_forms_organizing.climate_justice==1 |
-  #              q4_forms_organizing.countering_anti_gender==1 |
-  #              q4_forms_organizing.anti_militarization==1 |
-  #              q4_forms_organizing.holistic_safety_protection_collective_care==1 |
-  #              q4_forms_organizing.resisting_war_on_drugs==1 |
-  #              q4_forms_organizing.economic_justice==1 |
-  #              q4_forms_organizing.crisis_response==1 |
-  #              q4_forms_organizing.digital_rights==1 |
-  #              q4_forms_organizing.racial_justice==1 |
-  #              q4_forms_organizing.information_media_freedom==1 |
-  #              q4_forms_organizing.harm_reduction==1 |
-  #              q4_forms_organizing.pleasure_bodily_care==1) ~1, TRUE ~0)) 
-  # 
-=======
   mutate(q4_grouped_focus=case_when
          (
            (
@@ -186,7 +155,6 @@ base<-base %>%
   mutate(q4_awid_crisis=case_when(q4_forms_organizing.anti_militarization==1 | q4_forms_organizing.crisis_response==1 ~1, TRUE ~0)) %>%
   mutate(q4_awid_harm=case_when(q4_forms_organizing.harm_reduction==1 |q4_forms_organizing.resisting_war_on_drugs==1  ~1, TRUE ~0))
 
->>>>>>> a6e270c2bcaa3a33cbd76d50fc30eea4ca460245
 
 
 
@@ -202,7 +170,7 @@ base<-base %>%
 # 1,000,001+ USD
 
 
-witm<-witm %>%
+base<- base %>%
   mutate(q10_budget_grp_2021=case_when
          (
            (q10_budget_year_2021=="(g) 100001 - 250000"  |
@@ -213,7 +181,7 @@ witm<-witm %>%
               q10_budget_year_2021=="(l) > 4000001") ~ "(i) 1,000,001 + USD",
            TRUE ~ q10_budget_year_2021))  
 
-witm<-witm %>%
+base<- base %>%
   mutate(q10_budget_grp_2022=case_when
          (
            (q10_budget_year_2022=="(g) 100001 - 250000"  |
@@ -224,7 +192,7 @@ witm<-witm %>%
               q10_budget_year_2022=="(l) > 4000001") ~ "(i) 1,000,001 + USD",
            TRUE ~ q10_budget_year_2022))  
 
-witm<-witm %>%
+base<- base %>%
   mutate(q10_budget_grp_2023=case_when
          (
            (q10_budget_year_2023=="(g) 100001 - 250000"  |
@@ -236,25 +204,33 @@ witm<-witm %>%
            TRUE ~ q10_budget_year_2023))  
 
 
-witm<-witm %>% 
-  mutate(q42_aspirational_budget=case_when(
-  q42_aspirational_budget=="zero_budget" ~  "(a) Zero budget",
-  q42_aspirational_budget=="<5000" ~  "(b) <5,000 USD",
-  q42_aspirational_budget=="<10000" ~  "(c) 5,001-10,000 USD",
-  q42_aspirational_budget=="<30000" ~  "(d) 10,001-30,000 USD",
-  q42_aspirational_budget=="<50000" ~  "(e) 30,001 -50,000 USD",
-  q42_aspirational_budget=="<100000" ~  "(f) 50,001 - 100,000 USD",
-  q42_aspirational_budget=="<250000" | q42_aspirational_budget=="<500000" ~  "(g) 100,001 - 500,000 USD",
-  q42_aspirational_budget=="<1000000" ~ "(h) 500,001 - 1,000,000 USD",
-  q42_aspirational_budget=="between_2m_and_4m_usd" | q42_aspirational_budget=="greater_than_4m_usd" ~ "(i) 1,000,001 + USD",
-  TRUE ~  NA))
-
-witm <- witm %>% rename(q2_description=q1_description)
-witm <- witm %>% rename(q2_description_group=q1_description_group)
-
-witm <- witm %>% rename(q3_name=q2_name)
-
-write.csv(witm, "Data/WITM_cleaned_20240912.csv",row.names=FALSE)
-
-                        
-
+# delete this after
+# base<- base %>%
+#   mutate(q10_budget_grp_2021=case_when
+#          (
+#            (q10_budget_year_2021=="(h) 250001 - 500000"  |
+#               q10_budget_year_2021=="(i) 500001 - 1000000" | 
+#               q10_budget_year_2021=="(j) 1000001 - 2000000" |
+#               q10_budget_year_2021=="(k) 2000001 - 4000000" |
+#               q10_budget_year_2021=="(l) > 4000001") ~ "(h) > 250001",
+#            TRUE ~ q10_budget_year_2021))  
+# 
+# base<- base %>%
+#   mutate(q10_budget_grp_2022=case_when
+#          (
+#            (q10_budget_year_2022=="(h) 250001 - 500000"  |
+#               q10_budget_year_2022=="(i) 500001 - 1000000" | 
+#               q10_budget_year_2022=="(j) 1000001 - 2000000" |
+#               q10_budget_year_2022=="(k) 2000001 - 4000000" |
+#               q10_budget_year_2022=="(l) > 4000001") ~ "(h) > 250001",
+#            TRUE ~ q10_budget_year_2022))  
+# 
+# base<- base %>%
+#   mutate(q10_budget_grp_2023=case_when
+#          (
+#            (q10_budget_year_2023=="(h) 250001 - 500000"  |
+#               q10_budget_year_2023=="(i) 500001 - 1000000" | 
+#               q10_budget_year_2023=="(j) 1000001 - 2000000" |
+#               q10_budget_year_2023=="(k) 2000001 - 4000000" |
+#               q10_budget_year_2023=="(l) > 4000001") ~ "(h) > 250001",
+#            TRUE ~ q10_budget_year_2023))  
